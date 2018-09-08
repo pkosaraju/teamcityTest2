0703aae6c35b39129fb0b741e0cdc01283100bf5
kvprasanth <kosaraju.prasanth@gmail.com>
2018-09-09 01:44:55 +1000

    nunit test

---
+++ TTest2/UTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestCompareOne()
+        {
+            int x = 5;
+            int y = 5;
+            Assert.AreEqual(x, y);
+        }
+
+        [Test]
+        public void TestCompareTwo()
+        {
+            int x = 5;
+            int y = 9;
+            Assert.AreEqual(x, y);
+        }
+    }
+}
